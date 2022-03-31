7b632f2fb718ba87cfbdc516cbd0ae3f7648fc4f
syddakidd <79600955+syddakidd@users.noreply.github.com>
2022-03-31 11:42:50 +0200

    Second Commit Create project file, main, templates folder...

--- main.py
+++ main.py
@@ -8,7 +8,24 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route('/recipes') #Subseite für die Rezepte
+# @app.route('/recipes') #Subseite für die Rezepte
+
+def recipe():
+    nutrition_facts = [
+    {
+        "name" : "Banane",
+        "kcal" : "Chur",
+        "prot" : "4.0",
+        "fett" : "Calanda",
+        "kohl" : 2.80
+    },
+    {
+        "name" : "Banane",
+        "kcal" : "Chur",
+        "prot" : "4.0",
+        "fett" : "Calanda",
+        "kohl" : 2.80
+    }
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
